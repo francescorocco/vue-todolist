0cfe4b1ee2a3e12e5b4b4c4cc124d0e02363db2f
Francesco Rocco <francescorocco97@gmail.com>
2023-03-16 17:29:41 +0100

    bonus aggiunti

--- js/main.js
+++ js/main.js
@@ -29,23 +29,26 @@ const { createApp } = Vue
       remouveTask(index){
         this.taskList.splice(index, 1);
       },
+
       pushTask(){
         if(this.newTask.length >= 5){
-          let newObject = {
-            text: this.newTask,
-            done: false,
-          } 
+            let newObject = {
+              text: this.newTask,
+              done: false,
+            } 
           this.taskList.push(newObject);
           this.newTask = '';
         }else{
           alert('Inserisci almeno 5 caratteri');
           this.newTask = '';
         }
+
+      },
+      doneUndone(element){
+        element.done = !element.done
       }
     }
   }).mount('#app')
 
 
 
-//MILESTONE 3
-// Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
