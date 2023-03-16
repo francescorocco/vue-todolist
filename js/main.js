54f96a6f79ddf0d5b28af2dd105cdecf4be4c5ce
Francesco Rocco <francescorocco97@gmail.com>
2023-03-16 17:12:08 +0100

    Milestone 3 completo

--- js/main.js
+++ js/main.js
@@ -20,17 +20,32 @@ const { createApp } = Vue
             text: 'Task 4',
             done: false,
           },
-        ]
+        ],
+        
+        newTask:'',
       }
     },
     methods:{
       remouveTask(index){
         this.taskList.splice(index, 1);
       },
+      pushTask(){
+        if(this.newTask.length >= 5){
+          let newObject = {
+            text: this.newTask,
+            done: false,
+          } 
+          this.taskList.push(newObject);
+          this.newTask = '';
+        }else{
+          alert('Inserisci almeno 5 caratteri');
+          this.newTask = '';
+        }
+      }
     }
   }).mount('#app')
 
 
 
-  // MILESTONE 2
-  // Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+//MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
