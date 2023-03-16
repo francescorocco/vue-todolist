d154e608508c53155f144a83863fc29d5926fb1c
Francesco Rocco <francescorocco97@gmail.com>
2023-03-16 16:50:13 +0100

    Milestone 2 completo

--- js/main.js
+++ js/main.js
@@ -22,15 +22,15 @@ const { createApp } = Vue
           },
         ]
       }
+    },
+    methods:{
+      remouveTask(index){
+        this.taskList.splice(index, 1);
+      },
     }
   }).mount('#app')
 
 
 
-
-
-//  - text, una stringa che indica il testo del todo
-//  - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
-// MILESTONE 1
-// Stampare all’interno di una lista HTML un item per ogni todo.
-// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+  // MILESTONE 2
+  // Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
